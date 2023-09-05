ed21f85d02846e83002a84aee4c73b804ef96495
Rodrigo <rodicom10@gmail.com>
2023-09-04 21:35:04 -0300

    Entrega 2 (fix)

--- Registro.py
+++
@@ -1,9 +0,0 @@
-class Registro:
-    def __init__(self, id, nombre, email):
-        self.tamaño = 291
-        self.id = id
-        self.nombre = nombre
-        self.email = email
-
-    def contenido(self):
-        print(f"{self.id} {self.nombre} {self.email}")
