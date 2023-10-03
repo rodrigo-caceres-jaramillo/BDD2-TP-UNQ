2c379b6326278d42983896caa5d30155efd62ffa
Rodrigo <rodicom10@gmail.com>
2023-10-02 23:52:03 -0300

    main fix

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from elementos import MaquinaVirtual
 import sys
 
-#if len(sys.argv) < 2:
-#   print("ERROR: Introducir un archivo valido")
-#  sys.exit()
+if len(sys.argv) < 2:
+    print("ERROR: Introducir un archivo valido")
+    sys.exit()
    
-#nombre_archivo = sys.argv[1]
+nombre_archivo = sys.argv[1]
 
-maquinaVirtual = MaquinaVirtual("archivo")
+maquinaVirtual = MaquinaVirtual(nombre_archivo)
 maquinaVirtual.iniciar()
